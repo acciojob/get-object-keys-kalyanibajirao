eb5229210d0f211825afa38e5cb3abf359d01674
Kalyani Bajirao <95849305+kalyanibajirao@users.noreply.github.com>
2024-06-13 19:28:50 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your JS code here. If required.
+function Student(name) {
+    this.name = name;
+}
+
+// Add a method to the Object prototype
+Object.prototype.getKeys = function() {
+    return Object.keys(this);
+};
